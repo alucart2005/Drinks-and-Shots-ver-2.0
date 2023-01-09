f427758cb6cd75ccdf0269ab4a02d14490b86805
Napoleon Anaya Romero <113813382+alucart2005@users.noreply.github.com>
2023-01-09 05:42:40 -0500

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
         <div className="screen">
           <div className="card">
             <h2>Sorry, at this moment</h2>
-            <img src="./src/assets/not-exist.jpg" alt="" />
+            <img src="./public/not-exist.jpg" alt="" />
             <h2>we don´t have this drink</h2>
           </div>
         </div>
